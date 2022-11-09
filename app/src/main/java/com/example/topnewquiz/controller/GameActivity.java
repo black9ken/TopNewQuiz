6a51b503e0dabbad08176a240a035a7d11e7f6a9
black9ken <kennedy119@hotmail.fr>
2022-11-09 20:13:34 +0100

    second commit -ajout de questions -ajout test image

--- app/src/main/java/com/example/topnewquiz/controller/GameActivity.java
+++ app/src/main/java/com/example/topnewquiz/controller/GameActivity.java
@@ -85,46 +85,124 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             mRemainingQuestionCount = savedInstanceState.getInt(BUNDLE_STATE_QUESTION);
         } else {
             mScore = 0;
-            mRemainingQuestionCount = 2;
+            mRemainingQuestionCount = 10;
         }
 
     }
 
     private QuestionBank generateQuestions(){
         Question question1 = new Question(
-                "Who is the creator of Android?",
+                "Au début de la série, quelle famille est alliée aux Stark",
                 Arrays.asList(
-                        "Andy Rubin",
-                        "Steve Wozniak",
-                        "Jake Wharton",
-                        "Paul Smith"
+                        "BARATHEON",
+                        "LANNISTER",
+                        "TYRELL",
+                        "GREYJOY"
                 ),
                 0
         );
 
         Question question2 = new Question(
-                "When did the first man land on the moon?",
+                "Qui est le roi qui a fabriqué le trône de fer",
                 Arrays.asList(
-                        "1958",
-                        "1962",
-                        "1967",
-                        "1969"
+                        "VISERYS TARGARYEN",
+                        "AEMON TARGARYEN",
+                        "AEGON TARGARYEN",
+                        "AERYS TARGARYEN"
                 ),
-                3
+                2
         );
 
         Question question3 = new Question(
-                "What is the house number of The Simpsons?",
+                "Le combat entre Birnne de Torth et le Limier se déroule dans la saison...",
                 Arrays.asList(
-                        "42",
-                        "101",
-                        "666",
-                        "742"
+                        "3",
+                        "4",
+                        "5",
+                        "6"
+                ),
+                1
+        );
+
+        Question question4 = new Question(
+                "Quel est le personnage qui a fait le plus d'apparition dans la série ?",
+                Arrays.asList(
+                        "JON SNOW",
+                        "DAENERYS",
+                        "CERCEI",
+                        "TYRION"
                 ),
                 3
         );
 
-        return new QuestionBank(Arrays.asList(question1, question2, question3));
+        Question question5 = new Question(
+                "Viserys Targaryen reçoit une couronne d'or par le Khal Drogon dans la...",
+                Arrays.asList(
+                        "saison 1 épisode 9",
+                        "saison 1 épisode 6",
+                        "saison 2 épisode 1",
+                        "saison 2 épisode 2"
+                ),
+                1
+        );
+
+        Question question6 = new Question(
+                "Lequel de ces personnages est surnommé le \"Silure\"",
+                Arrays.asList(
+                        "stannis baratheon",
+                        "brynden tully",
+                        "jorah mormont",
+                        "petyr baelish"
+                ),
+                1
+        );
+
+        Question question7 = new Question(
+                "Comment s'appelle l'auteur des livres \"Game of Thrones\"",
+                Arrays.asList(
+                        "Michael hirst",
+                        "Walter white",
+                        "Vince gilligan",
+                        "George r.r. martin"
+                ),
+                3
+        );
+
+        Question question8 = new Question(
+                "Lequel de ces personnages a dit \"Tous les Hommes doivent mourir. Mais nous ne sommes pas des Hommes.\"",
+                Arrays.asList(
+                        "cercei lannister",
+                        "arya stark",
+                        "olenna tyrell",
+                        "daenerys targaryen"
+                ),
+                3
+        );
+
+        Question question9 = new Question(
+                "Lequel de ces personnages a dit \"De quel droit le loup juge t-il le lion\"",
+                Arrays.asList(
+                        "jaime lannister",
+                        "tywin lannister",
+                        "cercei lannister",
+                        "joffrey baratheon"
+                ),
+                0
+        );
+
+        Question question10 = new Question(
+                "Quel est le nom du premier spin off de Game of Thrones",
+                Arrays.asList(
+                        "House of fire",
+                        "Blood and fire",
+                        "House of the dragon",
+                        "Wild winter"
+                ),
+                2
+        );
+
+
+        return new QuestionBank(Arrays.asList(question1, question2, question3, question3, question4, question5, question6, question7, question8, question9, question10));
     }
 
     private void displayQuestion(final Question question) {
